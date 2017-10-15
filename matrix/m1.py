b0822e820103ec6308c90c29a126ad4a95e1adbb
happy-beans <fukumame.works@gmail.com>
2017-10-16 04:17:33 +0900

    行列

--- matrix/m1.py
+++ matrix/m1.py
@@ -3,39 +3,33 @@
 
 import numpy as np
 
+def p(name, ar):
+    print("{name} :".format(name=name))
+    print("{value}".format(value=ar))
+    print("")
+
 if __name__ == "__main__":
 
     # サンプルの行列
     ar = np.random.randint(1,10,4).reshape(2,2)
+    br = np.random.randint(1,10,4).reshape(2,2)
 
     # 単位行列
     I = np.identity(2)
-
     # 零行列
     O = np.zeros(4).reshape(2,2)
 
-    # 行列が以下で定義されるとき
-    print("ar = ")
-    print(ar)
-    print("")
-    print("I = ")
-    print(I)
-    print("")
-    print("O = ")
-    print(O)
-    print("")
+    p("ar", ar)
+    p("br", br)
+    p("I", I)
+    p("O", O)
 
-    # 単位行列との演算
-    print("1. ar.I")
-    print(ar.dot(I))
-    print("")
-    print("2. I.ar")
-    print(I.dot(ar))
-    print("")
+    p("ar + br", ar + br)
+    p("ar.br", ar.dot(br))
+    p("br.ar", br.dot(ar))
 
-    # 零行列との演算
-    print("3. ar.O")
-    print(ar.dot(O))
-    print("")
-    print("4. O.ar")
-    print(O.dot(ar))
+    p("ar.I", ar.dot(I))
+    p("I.ar", I.dot(ar))
+
+    p("ar.O", ar.dot(O))
+    p("O.ar", O.dot(ar))
