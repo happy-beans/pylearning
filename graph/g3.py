749158b03446f79572db355f24654bbc356f92a4
happy-beans <fukumame.works@gmail.com>
2017-10-22 11:02:54 +0900

    pyplotグラフ描画

---
+++ graph/g3.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# g3.py
+
+"""
+matplotlib appearance
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def makeGraph():
+    x = np.linspace(-10, 10, 100)
+    return x, x**2
+
+def makeFigure():
+    fig = plt.figure()
+
+    # figure() のタイトル
+    fig.suptitle("g3.py")
+
+    return fig
+
+def makeAxes(fig):
+    ax = fig.add_subplot(1,1,1)
+
+    # グラフ・色・凡例
+    x, y = makeGraph()
+    ax.plot(x, y, color='b', label='$y=x^2$')
+
+    # 凡例を表示
+    ax.legend(loc='lower right')
+
+    # axesタイトル
+    ax.set_title("$y=x^2$")
+
+    # x軸, y軸
+    ax.set_xlabel("axes x")
+    ax.set_ylabel("y label")
+
+    # グラフの表示範囲（定義域、値域）
+    ax.set_xlim([-10.0, 10.0])
+    ax.set_ylim([0.0, 10.0])
+
+    return ax
+
+if __name__ == "__main__":
+
+    fig = makeFigure()
+    ax = makeAxes(fig)
+
+    plt.savefig("./img/g3.png")
+    plt.show()
